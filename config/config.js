a81189542d8318a80f4943ba5aff00316a30d3b5
Carlo Toledo <ctoledo2@my.centennialcollege.ca>
2023-11-15 16:15:26 -0500

    completed user api

--- config/config.js
+++ config/config.js
@@ -15,8 +15,11 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
     mongoUri:   process.env.MONGODB_URI || 
-                "mongodb+srv://Blessing:qzzJjISAcwfPklIo@cluster0.txlxgvp.mongodb.net/Skeleton?retryWrites=true&w=majority"||
+                "mongodb+srv://ctoledo2:DyH9R5N0Uk88bOEf@cluster0.nl2mkqk.mongodb.net/?retryWrites=true&w=majority"||
                 process.env.MONGO_HOST ||
                 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject' 
     }
     export default config
+
+
+// "mongodb+srv://ctoledo2:DyH9R5N0Uk88bOEf@cluster0.nl2mkqk.mongodb.net/?retryWrites=true&w=majority"
